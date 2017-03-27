1176f9307a39bbe77fdbd16542af7c674ad1d833
root <root@moussa-Inspiron-3537>
2017-03-27 22:24:47 +0200

    new commit

--- index.php
+++ index.php
@@ -1,9 +1,15 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
+<?php
+    include 'models/Tester.php';
+    include 'models/Database.php';
+    
+    try {
+            $DB = new Database('include/vars.php');
+        } catch (Exception $exc) 
+        {
+            echo $exc->getMessage();
+        }
+    
+?>
 <html>
     <head>
         <meta charset="UTF-8">
