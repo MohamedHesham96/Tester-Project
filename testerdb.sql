3ccd8f0e97a2a7f044f13bad0cce35b4d20e555e
H <H@192.168.1.118>
2017-04-09 21:33:05 +0200

    Update Database (2)

--- testerdb.sql
+++ testerdb.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 09, 2017 at 02:17 PM
+-- Generation Time: Apr 09, 2017 at 07:17 PM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 7.0.8
 
@@ -29,8 +29,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `following` (
   `id` int(11) NOT NULL,
   `doctor_id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL
+  `student_name` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `following`
+--
+
+INSERT INTO `following` (`id`, `doctor_id`, `student_name`) VALUES
+(0, 1, 'mona'),
+(1, 2, 'mohamed');
 
 -- --------------------------------------------------------
 
@@ -40,10 +48,21 @@ CREATE TABLE `following` (
 
 CREATE TABLE `history` (
   `id` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL,
   `quiz_id` int(11) NOT NULL,
-  `submit_id` int(11) NOT NULL
+  `submit_id` int(11) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
+  `student_name` varchar(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `history`
+--
+
+INSERT INTO `history` (`id`, `quiz_id`, `submit_id`, `doctor_id`, `student_name`) VALUES
+(0, 0, 3, 1, 'mona'),
+(1, 1, 1, 2, 'mohamed'),
+(2, 0, 2, 1, 'mohamed'),
+(4, 1, 4, 2, 'mona');
 
 -- --------------------------------------------------------
 
@@ -52,7 +71,7 @@ CREATE TABLE `history` (
 --
 
 CREATE TABLE `questions` (
-  `id` int(11) NOT NULL,
+  `questio_id` int(11) NOT NULL,
   `quiz_id` int(11) NOT NULL,
   `header` text NOT NULL,
   `answer_1` varchar(100) NOT NULL,
@@ -62,6 +81,14 @@ CREATE TABLE `questions` (
   `correct_answer` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `questions`
+--
+
+INSERT INTO `questions` (`questio_id`, `quiz_id`, `header`, `answer_1`, `answer_2`, `answer_3`, `answer_4`, `correct_answer`) VALUES
+(0, 1, 'are you a gril ??', 'yes', 'no', NULL, NULL, ''),
+(2, 0, 'are you human ? ', 'yes', 'very yes', 'no', 'sure no', 'yes');
+
 -- --------------------------------------------------------
 
 --
@@ -69,13 +96,20 @@ CREATE TABLE `questions` (
 --
 
 CREATE TABLE `quizzes` (
-  `id` int(11) NOT NULL,
-  `name` varchar(100) NOT NULL,
-  `user_id` int(11) NOT NULL,
-  `password` int(11) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `code` int(10) NOT NULL
+  `quiz_id` int(11) NOT NULL,
+  `quiz_name` varchar(100) NOT NULL,
+  `doctor_id` int(11) NOT NULL,
+  `password` int(11) DEFAULT NULL,
+  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `quizzes`
+--
+
+INSERT INTO `quizzes` (`quiz_id`, `quiz_name`, `doctor_id`, `password`, `time`) VALUES
+(0, 'test#1', 1, 2222, '2017-04-09 14:19:37'),
+(1, 'test#2', 2, 5555, '2017-04-09 17:01:51');
 
 -- --------------------------------------------------------
 
@@ -85,10 +119,21 @@ CREATE TABLE `quizzes` (
 
 CREATE TABLE `submits` (
   `id` int(11) NOT NULL,
-  `mark` int(11) NOT NULL,
   `student_id` int(11) NOT NULL,
+  `quiz_id` int(11) NOT NULL,
+  `mark` int(11) NOT NULL,
   `time` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `submits`
+--
+
+INSERT INTO `submits` (`id`, `student_id`, `quiz_id`, `mark`, `time`) VALUES
+(1, 3, 0, 30, '00:08:18'),
+(2, 4, 1, 25, '00:10:10'),
+(3, 3, 0, 25, '00:08:18'),
+(4, 4, 1, 22, '00:10:07');
 
 -- --------------------------------------------------------
 
@@ -104,7 +149,7 @@ CREATE TABLE `users` (
   `type` varchar(10) NOT NULL,
   `age` int(3) NOT NULL,
   `country` varchar(50) NOT NULL,
-  `gender` varchar(5) NOT NULL,
+  `gender` varchar(6) NOT NULL,
   `phone` varchar(20) NOT NULL,
   `image` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -114,9 +159,11 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `username`, `password`, `email`, `type`, `age`, `country`, `gender`, `phone`, `image`) VALUES
-(0, 'ahmed\r\n', '213', 'hany@gmail.com', 'doctor', 35, 'egypt', 'male', '011000066666', ''),
-(1, 'hany', '213', 'mr@gmail.com', 'doctor', 35, 'egypt', 'male', '011000066666', ''),
-(2, 'maged', '213', 'medo@mal.com', 'admin', 21, 'egypt', 'male', '220001111000', '');
+(1, 'dr.ahmed\n', '213', 'hany@gmail.com', 'doctor', 35, 'egypt', 'male', '011000066666', ''),
+(2, 'dr.hazem', '252', 'h@gmail.com', 'doctor', 35, 'egypt', 'male', '021515151', ''),
+(3, 'mohamed', '213', 'medo@gmail.com', 'student', 20, 'egypt', 'male', '012022255', ''),
+(4, 'mona', '213', 'mona@gmail.com', 'student', 20, 'egypt', 'femal', '012000000', ''),
+(5, 'maged', '213', 'medo@mal.com', 'admin', 21, 'egypt', 'male', '220001111000', '');
 
 --
 -- Indexes for dumped tables
@@ -134,7 +181,7 @@ ALTER TABLE `following`
 --
 ALTER TABLE `history`
   ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`student_id`),
+  ADD UNIQUE KEY `doctor_id` (`id`) USING BTREE,
   ADD KEY `quiz_id` (`quiz_id`),
   ADD KEY `submit_id` (`submit_id`);
 
@@ -142,23 +189,21 @@ ALTER TABLE `history`
 -- Indexes for table `questions`
 --
 ALTER TABLE `questions`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`questio_id`),
   ADD KEY `quiz_id` (`quiz_id`);
 
 --
 -- Indexes for table `quizzes`
 --
 ALTER TABLE `quizzes`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `code` (`code`),
-  ADD KEY `user_id` (`user_id`);
+  ADD PRIMARY KEY (`quiz_id`),
+  ADD KEY `doctor_id` (`doctor_id`) USING BTREE;
 
 --
 -- Indexes for table `submits`
 --
 ALTER TABLE `submits`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `user_id` (`student_id`);
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Indexes for table `users`
@@ -173,10 +218,15 @@ ALTER TABLE `users`
 --
 
 --
--- AUTO_INCREMENT for table `quizzes`
+-- AUTO_INCREMENT for table `submits`
 --
-ALTER TABLE `quizzes`
-  MODIFY `code` int(10) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `submits`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
