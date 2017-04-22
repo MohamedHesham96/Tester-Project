0f92d8d7665587d386ebc25bcbfeaecf90a36887
MohamedHesham96 <mohamed.hesham.fci@gmail.com>
2017-04-23 02:45:33 +0300

    Correct Input Names

--- view/subscribes.php
+++ view/subscribes.php
@@ -3,6 +3,11 @@
         <link href="../recources/css/bootstrap.css" rel="stylesheet" type="text/css"/>
         <link href="../recources/css/style1.css" rel="stylesheet" type="text/css"/>
 
+
+
+
+
+
         <meta charset="utf-8"/>
     </head>
     <body >
