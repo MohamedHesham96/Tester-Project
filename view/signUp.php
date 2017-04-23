ab27004c5b735f87b3cd3176bf48b7005dc9a3ed
MohamedHesham96 <mohamed.hesham.fci@gmail.com>
2017-04-23 17:55:47 +0300

    Delete Document file

--- view/signUp.php
+++ view/signUp.php
@@ -21,12 +21,14 @@
     <body>
         <div class="image-container set-full-height" style="background-image: url('img/wizard.jpg')">
 
+            <!--   Big container   -->
             <div class="container">
                 <div class="row">
-
                     <div class="col-sm-8 col-sm-offset-2">
 
+                        <!--      Wizard container        -->
                         <div class="wizard-container">
+
                             <div class="card wizard-card" data-color="orange" id="wizardProfile">
                                 <form action="" method="">
                                     <!--        You can switch ' data-color="orange" '  with one of the next bright colors: "blue", "green", "orange", "red"          -->
@@ -42,12 +44,12 @@
                                         <ul>
                                             <li><a href="#about" data-toggle="tab">About</a></li> 
                                             <li><a href="#account" data-toggle="tab">Account</a></li>
-                                            <li><a href="#address" data-toggle="tab">Address</a></li>
+                                            <li><a href="#address" data-toggle="tab">Info</a></li>
                                         </ul>
 
                                     </div>
-
                                     <div class="tab-content">
+
                                         <div class="tab-pane" id="about">
                                             <div class="row">
                                                 <br>    <div class="col-sm-4 col-sm-offset-1">
@@ -95,10 +97,9 @@
                                             <div class="row">
 
                                                 <div class="col-sm-10 col-sm-offset-1">
-
                                                     <div class="col-sm-4">
                                                         <div class="choice" data-toggle="wizard-checkbox">
-                                                            <input type="checkbox" name="type" value="solver">
+                                                            <input type="radio" name="type" value="solver">
                                                             <div class="icon">
                                                                 <i class="fa fa-terminal"></i>
                                                             </div>
@@ -108,7 +109,7 @@
                                                     </div>
                                                     <div class="col-sm-4">
                                                         <div class="choice" data-toggle="wizard-checkbox">
-                                                            <input type="checkbox" name="type" value="maker">
+                                                            <input type="radio" name="type" value="maker">
                                                             <div class="icon">
                                                                 <i class="fa fa-laptop"></i>
                                                             </div>
@@ -116,6 +117,8 @@
                                                         </div>
 
                                                     </div>
+                                              
+                                                  
                                                 </div>
 
                                             </div>
@@ -136,7 +139,7 @@
                                                         <br>
                                                         <br>
                                                         <label>Birth Day<small></small></label>
-                                                        <input class="form-control" type="date" placeholder="Enter Your Birth Date"  name="bday" required>
+                                                        <input class="form-control" type="date" placeholder="Enter Your Birth Date"  name="birth_day">
                                                         <br>
                                                         <label>Phone <small></small></label>
                                                         <input class="form-control" type="tel" placeholder="Enter Your Phone Number" name="phone">
@@ -410,19 +413,18 @@
                                             </div>
                                         </div>
                                         <br>                                        <br>
-
-                                        <div class="wizard-footer height-wizard">
-                                            <div class="pull-right">
-                                                <input type='button' class='btn btn-next btn-fill btn-warning btn-wd btn-sm' style="font-weight: bold;"name='next' value='Next' />
-                                                <input type='button' class='btn btn-finish btn-fill btn-warning btn-wd btn-sm' name='finish' value='Finish' />
-
-                                            </div>
-
-                                            <div class="pull-left">
-                                                <input type='button' class='btn btn-previous btn-fill btn-default btn-wd btn-sm' name='previous' value='Previous' />
-                                            </div>
-                                            <div class="clearfix"></div>
+                                    </div>
+                                    <div class="wizard-footer height-wizard">
+                                        <div class="pull-right">
+                                            <input type='button' class='btn btn-next btn-fill btn-warning btn-wd btn-sm' style="font-weight: bold;"name='next' value='Next' />
+                                            <input type='button' class='btn btn-finish btn-fill btn-warning btn-wd btn-sm' name='finish' value='Finish' />
                                         </div>
+
+                                        <div class="pull-left">
+                                            <input type='button' class='btn btn-previous btn-fill btn-default btn-wd btn-sm' name='previous' value='Previous' />
+                                        </div>
+                                        <div class="clearfix"></div>
+                                    </div>
 
                                 </form>
                             </div>
@@ -435,18 +437,19 @@
 
 
         </div>
+    </div>
 
-    </body>
+</body>
 
-    <!--   Core JS Files   -->
-    <script src="../recources/js/jquery-2.2.4.min.js" type="text/javascript"></script>
-    <script src="../recources/js/bootstrap.min.js" type="text/javascript"></script>
-    <script src="../recources/js/jquery.bootstrap.wizard.js" type="text/javascript"></script>
+<!--   Core JS Files   -->
+<script src="../recources/js/jquery-2.2.4.min.js" type="text/javascript"></script>
+<script src="../recources/js/bootstrap.min.js" type="text/javascript"></script>
+<script src="../recources/js/jquery.bootstrap.wizard.js" type="text/javascript"></script>
 
-    <!--  Plugin for the Wizard -->
-    <script src="../recources/js/gsdk-bootstrap-wizard.js"></script>
+<!--  Plugin for the Wizard -->
+<script src="../recources/js/gsdk-bootstrap-wizard.js"></script>
 
-    <!--  More information about jquery.validate here: http://jqueryvalidation.org/  -->
-    <script src="../recources/js/jquery.validate.min.js"></script>
+<!--  More information about jquery.validate here: http://jqueryvalidation.org/  -->
+<script src="../recources/js/jquery.validate.min.js"></script>
 
 </html>
