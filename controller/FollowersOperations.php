93fc8d8f24fdbb3bd77027f7df76f3e40fe7cc67
MohamedHesham96 <mohamed.hesham.fci@gmail.com>
2017-04-23 03:44:08 +0300

    Add Followers Pages (Class, Page)

---
+++ controller/FollowersOperations.php
@@ -0,0 +1,25 @@
+<?php
+
+class HistoryOperations {
+
+    public static function viewAllQuizzes() {
+
+        include '../include/vars.php';
+
+        $conn = new mysqli($host, $username, $password, $dbname);
+        $query = "SELECT student_name FROM following WHERE doctor_id = 1";
+        
+        $result = mysqli_query($conn, $query);
+
+        if (mysqli_error($conn)) {
+            echo 'Followers Error !!';
+
+            return NULL;
+        } else {
+            return $result;
+        }
+    }
+
+}
+
+?>
---
+++ view/myFollowers.php
@@ -0,0 +1,68 @@
+<html>
+    <head>
+        <link href="../recources/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+        <link href="../recources/css/style1.css" rel="stylesheet" type="text/css"/>
+        <meta charset="utf-8"/>
+    </head>
+    <body >
+        <div class="log"> <button onclick="">log out</button></div>
+        <h4> welcome ** </h4>
+
+        <div class="nav">
+
+            <div class="container">
+
+                <ul>
+                    <li><a href="home.php" >HOME</a></li>
+                    <li><a href="history.php" class="active">History</a></li>
+                    <li><a href="subscribes.php">Subscribes</a></li>
+                    <li><a href="about.php">About</a></li>
+
+                </ul>
+            </div>
+        </div>
+
+
+
+        <h1> Your Exams <h1>
+                <div class="container">
+
+                    <table class="table-striped"> 
+                        <tr>	
+                            <td>Student Name</td>
+                            <td>Student E-Mail</td>
+                        </tr>
+
+
+                        <?php
+                        include '../controller/HistoryOperations.php';
+                        $reult = HistoryOperations::viewAllQuizzes();
+
+// check if the statment is true
+
+                        if (!$reult) {
+                            echo 'error2';
+                        } else {
+                            while ($row = mysqli_fetch_array($reult, 1)) {
+
+
+                                echo "<tr>";
+                                echo "<td>" . $row['quiz_id'] . "</td>";
+                                echo "<td>" . $row['quiz_name'] . "</td>";
+                                echo "<td>" . $row['username'] . "</td>";
+                                echo "<td>" . $row['mark'] . " / " . $row['full_mark'] . "</td>";
+                                echo "<td>" . $row['time'] . "</td>";
+                                if ($row['password']) {
+                                    echo "<td>" . '<img src=" ../recources/images/lock.png"  height="20" width="20">' . " </td>";
+                                } else {
+                                    echo "<td>" . '<img src=" ../recources/images/unlock.png"  height="22" width="22">' . " </td>";
+                                }
+                                echo "</tr>";
+                            }
+                        }
+                        ?>
+                    </table>
+                </div>
+                <link href="../recources/js/bootstrap.min.js" rel="stylesheet" type="text/javascript"/>
+                </body>
+                </html>
