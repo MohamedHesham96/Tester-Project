c7c46be26c5a41e05f6849359759549d86e2e1ad
MohamedHesham96 <mohamed.hesham.fci@gmail.com>
2017-04-26 01:38:15 +0300

    Done Some work

--- view/Quiz.php
+++ view/Quiz.php
@@ -39,56 +39,75 @@
 
 
     if (!$result) {
-        echo 'Quiz Page Error !!';
+    echo 'Quiz Page Error !!';
     } else {
 
-        while ($row = mysqli_fetch_array($result, 1)) {
+    $ans3state = "";  // to show the third answer of not 
+    $ans4state = "";  // to show the fourht answer of not 
 
-            if ($ansRow = mysqli_fetch_array($result2, 2))
-                shuffle($ansRow);
+    while ($row = mysqli_fetch_array($result, 1)) {
 
-            //get information from prevoius page which clicked by the user
-            ?>
+    if ($ansRow = mysqli_fetch_array($result2, 1)) {
 
-            <form action="mySubmit.php" method="GET">
+    /*   if (!$ansRow['answer_3']) {
+      $ans3state = "hidden";
+      $ansRow = array_rand($ansRow, 2);
+      } else if (!$ansRow['answer_4']) {
+      $ans4state = "hidden";
+      $ansRow = array_rand($ansRow, 3);
+      } else { */
 
-                <div class="form-group">
+    shuffle($ansRow);
+    }
+    
+    //get information from prevoius page which clicked by the user
+    ?>
 
-                    <label  id="title5" class="form-control">  
-                        <?php echo $row['header']; ?> 
-                    </label>  <![endif]-->  
-                    <input  name="<?php echo 'correct_ans' . $row['question_id']; ?>" type="hidden" value="<?php echo $row['correct_answer']; ?>"/>  
-                    <p>
-                        <input  name="<?php echo $row['question_id']; ?>" type="radio" class="field radio" value="<?php echo $ansRow[0]; ?>" readonly="readonly"/> 
-                        <label class="choice" > 
-                            <span class="choice__text notranslate"><?php echo $ansRow[0]; ?></span>
-                        </label>  
-                        <input name="<?php echo $row['question_id']; ?>" type="radio" class="field radio" value="<?php echo $ansRow[1]; ?>"  readonly="readonly"/>
-                        <label class="choice" > 
-                            <span class="choice__text notranslate"><?php echo $ansRow[1]; ?></span> 
-                        </label>  
-                        <input name="<?php echo $row['question_id']; ?>" type="radio" class="field radio" value="<?php echo $ansRow[2]; ?>"   readonly="readonly" /> 
-                        <label class="choice"  >
-                            <span class="choice__text notranslate"><?php echo $ansRow[2]; ?></span>
-                        </label> 
-              
-                        <input name="<?php echo $row['question_id']; ?>" type="radio" class="field radio" value="<?php echo $ansRow[3]; ?>"  readonly="readonly" /> 
-                        <label class="choice"  >
-                            <span class="choice__text notranslate"><?php echo $ansRow[3]; ?></span>
-                        </label> 
-              
+    <form action="mySubmit.php" method="GET">
 
-</p>
+        <div class="form-group">
+
+            <label  id="title5" class="form-control">  
+                <?php echo $row['header']; ?> 
+            </label>  <![endif]-->  
+            <input  name="<?php echo 'correct_ans' . $row['question_id']; ?>" type="hidden" value="<?php echo $row['correct_answer']; ?>"/>  
+            <p>
+                <input  name="<?php echo $row['question_id']; ?>" type="radio" class="field radio" value="<?php echo $ansRow[0]; ?>" readonly="readonly"/> 
+
+                <label class="choice" > 
+                    <span class="choice__text notranslate"><?php echo $ansRow[0]; ?></span>
+                </label>  
+
+                <input name="<?php echo $row['question_id']; ?>" type="radio" class="field radio" value="<?php echo $ansRow[1]; ?>"  readonly="readonly"/>
+
+                <label class="choice" > 
+                    <span class="choice__text notranslate"><?php echo $ansRow[1]; ?></span> 
+                </label>  
+
+                <input <?php echo $ans3state; ?> name="<?php echo $row['question_id']; ?>" type="radio" class="field radio" value="<?php echo $ansRow[2]; ?>"   readonly="readonly" /> 
+
+                <label <?php echo $ans3state; ?> class="choice"  >
+                    <span class="choice__text notranslate"><?php echo $ansRow[2]; ?></span>
+                </label> 
+
+                <input <?php echo $ans4state; ?> name="<?php echo $row['question_id']; ?>" type="radio" class="field radio" value="<?php echo $ansRow[3]; ?>"  readonly="readonly" /> 
+
+                <label <?php echo $ans4state; ?> class="choice"  >
+                    <span class="choice__text notranslate"><?php echo $ansRow[3]; ?></span>
+                </label> 
 
 
-                <?php } ?>
+            </p>
+
+
+            <?php } ?>
 
             <?php }
             ?>
 
             <br>    <br>
 
-            <input type="submit" value="Done!"> 
+            <input class="btn-success btn col-lg-2" type="submit" value="SUBMIT"> 
         </div> 
 
         <input  name="quizname" type="text" value="<?php echo $_GET['name']; ?>"  readonly="readonly" hidden/> 
