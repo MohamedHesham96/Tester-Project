6321aacdb50ca205b50ed5be7f52221814b16d20
mohamedabouzaid <mohmmdabouzaid@gmail.com>
2017-05-06 19:11:06 +0300

    Add new Quiz done

--- view/Header.php
+++ view/Header.php
@@ -1,25 +1,24 @@
 <?php
-    session_start();
-    if(!isset($_SESSION['username'])){
-        
-        echo '<script>alert("You must login in to continue :( ");</script>';
-        include 'Login.php';
-        die();
-    }
-    include  '../controller/MyProfileOperations.php';
+session_start();
+if (!isset($_SESSION['username'])) {
 
+    echo '<script>alert("You must login in to continue :( ");</script>';
+    include 'Login.php';
+    die();
+}
+include '../controller/MyProfileOperations.php';
 ?>
 <html>
     <head>
         <link href="../recources/css/bootstrap.css" rel="stylesheet" type="text/css"/>
+
+        <link href="../recources/css/style.css" rel="stylesheet" type="text/css"/>
+        <link href="../recources/css/style_1.css.css" rel="stylesheet" type="text/css"/>
         <link href="../recources/css/styletable.css" rel="stylesheet" type="text/css"/>
-        <style type="text/css">
-           
-        </style>
     </head>
 
     <body>
-     <?php
+        <?php
         if ($_SESSION['usertype'] == 'doctor') {
 
             $firstTab = 'DoctorHome';
@@ -33,7 +32,6 @@
             $firstTab = 'Home';
             $secondTab = 'History';
             $thirdTab = 'Subscribes';
-            
         } else if ($_SESSION['usertype'] == 'admin') {
 
             $firstTab = 'AdminHome';
@@ -42,130 +40,125 @@
         }
         ?>
 
-<nav class="navbar navbar-inverse " role="navigation" style="background:rgba(0, 0, 0, 0.62); border: 0; "> 
-    <div class="container">
-            <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
-                
-                <a class="navbar-brand" href="#">
+        <nav class="navbar navbar-inverse " role="navigation" style="background:rgba(0, 0, 0, 0.62); border: 0; "> 
+            <div class="container">
+                <!-- Brand and toggle get grouped for better mobile display -->
+                <div class="navbar-header">
 
-                <span style="color: #f5f5f5"> Qu<span class="glyphicon glyphicon glyphicon-hourglass" aria-hidden="true" style="color: rgba(61, 201, 179, 1)"></span>z</span>
-                </a>
-            </div>
-            <div class="collapse navbar-collapse navbar-ex1-collapse" style="color: #f5f5f5">
-                <ul class="nav navbar-nav">
-                    <li class="menu-item "><a href="<?php echo $firstTab ?>.php" >Home</a></li>
+                    <a class="navbar-brand" href="#">
+
+                        <span style="color: #f5f5f5"> Qu<span class="glyphicon glyphicon glyphicon-hourglass" aria-hidden="true" style="color: rgba(61, 201, 179, 1)"></span>z</span>
+                    </a>
+                </div>
+                <div class="collapse navbar-collapse navbar-ex1-collapse" style="color: #f5f5f5">
                     <ul class="nav navbar-nav">
-                        <li class="menu-item"><a href="<?php echo $secondTab . '.php' ?>" ><?php echo $secondTab ?></a></li>
-                        <li class="menu-item"><a href="<?php echo $thirdTab . '.php' ?>"><?php echo $thirdTab ?></a></li>
-          
-                        
-                            <?php
-                            if ($_SESSION['usertype'] == 'doctor') {
-                                $fourthPag = $forthTab . '.php';
-                                echo "<li class=\"menu-item\">"
-                                . "<a href='$fourthPag'>$forthTab</a>"
-                                . "</li>";
-                            }
-                            ?>
-
-                        
+                        <li class="menu-item "><a href="<?php echo $firstTab ?>.php" >Home</a></li>
                         <ul class="nav navbar-nav">
+                            <li class="menu-item"><a href="<?php echo $secondTab . '.php' ?>" ><?php echo $secondTab ?></a></li>
+                            <li class="menu-item"><a href="<?php echo $thirdTab . '.php' ?>"><?php echo $thirdTab ?></a></li>
+
+
+<?php
+if ($_SESSION['usertype'] == 'doctor') {
+    $fourthPag = $forthTab . '.php';
+    echo "<li class=\"menu-item\">"
+    . "<a href='$fourthPag'>$forthTab</a>"
+    . "</li>";
+}
+?>
+
 
                             <ul class="nav navbar-nav">
-                                <li class="menu-item"><a href="About.php">About</a></li>
+
+                                <ul class="nav navbar-nav">
+                                    <li class="menu-item"><a href="About.php">About</a></li>
+                                </ul>
+                                <ul class="nav navbar-nav navbar-right">
+
+                                    <li>
+                                        <form class="navbar-search navbar-form" method="get" action="SearchPage.php">
+                                            <input style="background:#f5f5f5 " class="form-control" placeholder="Quiz Name or Code..." name="Search" type="text" >
+                                            <input style="background:#f5f5f5 " class="form-control" type="submit" value="Search" name="SearchSub">
+                                        </form>
+                                    </li>
+
+                                </ul>
+                                <ul class="nav navbar-nav">
+                                    <li class="menu-item">
+                                    </li>
+                                </ul >
                             </ul>
-                            <ul class="nav navbar-nav navbar-right">
-                                
-                                <li>
-                                    <form class="navbar-search navbar-form" method="get" action="SearchPage.php">
-                                        <input style="background:#f5f5f5 " class="form-control" placeholder="Quiz Name or Code..." name="Search" type="text" >
-                                        <input style="background:#f5f5f5 " class="form-control" type="submit" value="Search" name="SearchSub">
-                                    </form>
-                                </li>
-                                
-                            </ul>
-                            <ul class="nav navbar-nav">
-                             <li class="menu-item">
-                             </li>
-                            </ul >
                         </ul>
                     </ul>
-                </ul>
-                <!-- DROPDOWN LOGIN STARTS HERE  -->
-                <ul id="signInDropdown" class="nav navbar-nav navbar-right">
-                 <li style="float: right; margin-top: 5px; margin-bottom: 5px">
-            
-              
-                   
-                
-            
-                     <a style=" color: #FFF;display: inline; margin:0;padding: 0" href="ProfilePage.php?name=<?php echo $_SESSION['username']  ?>"> <?php echo ucwords($_SESSION['username'])?> </a>
+                    <!-- DROPDOWN LOGIN STARTS HERE  -->
+                    <ul id="signInDropdown" class="nav navbar-nav navbar-right">
+                        <li style="float: right; margin-top: 5px; margin-bottom: 5px">
 
 
-                
-                    <a style="display: inline;margin:0;padding: 0"href="ProfilePage.php?name=<?php echo $_SESSION['username']  ?>">
-                        <?php    
-                            $user = $_SESSION['username'];
-                            $result = MyProfileOperations::getMyData($user);
-                            $row = mysqli_fetch_array($result);
-                            $profilephoto = $row['image'];
-                            if(empty($profilephoto))
-                            {
-                                echo '<img src="../recources/images/default-avatar.png" class="picture-src" height = "44" width="50"id="wizardPicturePreview" title=""/>';
 
-                            }
-                            else
-                            {    
-                                echo '<img style="border-radius: 60% ; display: inline" src="data:image/jpeg;base64,'.base64_encode($profilephoto).'" height = "44" width="50" class="img-thumnail" class="picture-src" id="wizardPicturePreview"/>'; 
-                            }
-                        ?>
-                    </a>
-                    
-                     <a style=" display: inline;margin:0;padding: 0" href="ProfilePage.php"> <?php echo '|'; ?> </a>
-                  
 
-                     <a style=" display: inline;margin:0;padding: 0" href="?page=Logout.php"> Log out </a>
-                        <?php
-                        if(isset($_GET['page']))
-                        {
-                            $page = $_GET['page']; 
-                            $url  = "../models/".$page;
-                            include $url;
-                        }   
-                    ?>
-                   
-                    </li>
 
-                    
-                </ul> 
-       
-         <!-- Navigation -->
-   
-           
-                           
-        
+                            <a style=" color: #FFF;display: inline; margin:0;padding: 0" href="ProfilePage.php?name=<?php echo $_SESSION['username'] ?>"> <?php echo ucwords($_SESSION['username']) ?> </a>
+
+
+
+                            <a style="display: inline;margin:0;padding: 0"href="ProfilePage.php?name=<?php echo $_SESSION['username'] ?>">
+<?php
+$user = $_SESSION['username'];
+$result = MyProfileOperations::getMyData($user);
+$row = mysqli_fetch_array($result);
+$profilephoto = $row['image'];
+if (empty($profilephoto)) {
+    echo '<img src="../recources/images/default-avatar.png" class="picture-src" height = "44" width="50"id="wizardPicturePreview" title=""/>';
+} else {
+    echo '<img style="border-radius: 60% ; display: inline" src="data:image/jpeg;base64,' . base64_encode($profilephoto) . '" height = "44" width="50" class="img-thumnail" class="picture-src" id="wizardPicturePreview"/>';
+}
+?>
+                            </a>
+
+                            <a style=" display: inline;margin:0;padding: 0" href="ProfilePage.php"> <?php echo '|'; ?> </a>
+
+
+                            <a style=" display: inline;margin:0;padding: 0" href="?page=Logout.php"> Log out </a>
+<?php
+if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+    $url = "../models/" . $page;
+    include $url;
+}
+?>
+
+                        </li>
+
+
+                    </ul> 
+
+                    <!-- Navigation -->
+
+
+
+
                 </div>
-                </div>
-
-     
-
-            
-
-
-      </nav>     
+            </div>
 
 
 
 
-                
-                    
-
-        
 
 
+        </nav>     
 
-        
+
+
+
+
+
+
+
+
+
+
+
     </body>
     <!--   Core JS Files   -->
     <script src="../recources/js/jquery-2.2.4.min.js" type="text/javascript"></script>
