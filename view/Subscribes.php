4b622798746d1e6261dee55c02336d86bfa3578d
mohamedhesham96 <H@192.168.1.113>
2017-05-06 21:35:55 +0300

    Correct Links

--- view/Subscribes.php
+++ view/Subscribes.php
@@ -1,10 +1,6 @@
 <?php include './Header.php'; ?>
 <html>
     <head>
-        <link href="../recources/css/bootstrap.css" rel="stylesheet" type="text/css"/>
-        <link href="../recources/css/style1.css" rel="stylesheet" type="text/css"/>
-        <link href="../recources/css/styletable.css" rel="stylesheet" type="text/css"/>
-
         <meta charset="utf-8"/>
     </head>
     <body >
@@ -12,38 +8,38 @@
         $studentName = $_SESSION['username'];
         ?>
         <h1> following </h1>
-                <div class="container">
+        <div class="">
 
-                    <table class="containerr"> 
+            <table class="containerr"> 
 
-                        <tr>
-                            <th>Doctor Photo</th>
-                            <th>Doctor name</th>
-                            <th>Doctor E-Mail</th>
-                        </tr>
+                <tr>
+                    <th>Doctor Photo</th>
+                    <th>Doctor name</th>
+                    <th>Doctor E-Mail</th>
+                </tr>
 
 
-                        <?php
-                        include '../controller/SubscribesOperations.php';
-                        $reult = SubscribesOperations::viewAllSubscribes($studentName);
+                <?php
+                include '../controller/SubscribesOperations.php';
+                $reult = SubscribesOperations::viewAllSubscribes($studentName);
 
 // check if the statment is true
 
-                        if (!$reult) {
-                            echo 'error2';
-                        } else {
-                            while ($row = mysqli_fetch_array($reult, 2)) {
-                                echo "<tr>";
-                                echo "<td>" . "Photo" . "</td>";
-                                echo '<td><a href="ProfilePage.php?outprofile=true&followstate=true&name=' . $row[1] . '">' . $row[1] . '</a></td>';
-                                echo "<td>" . $row[2] . "</td>";
-                                echo "</tr>";
-                            }
-                        }
-                        ?>
+                if (!$reult) {
+                    echo 'error2';
+                } else {
+                    while ($row = mysqli_fetch_array($reult, 2)) {
+                        echo "<tr>";
+                        echo "<td>" . "Photo" . "</td>";
+                        echo '<td><a href="ProfilePage.php?outprofile=true&followstate=true&name=' . $row[1] . '">' . $row[1] . '</a></td>';
+                        echo "<td>" . $row[2] . "</td>";
+                        echo "</tr>";
+                    }
+                }
+                ?>
 
-                    </table>
-                </div>
-                <link href="../recources/js/bootstrap.min.js" rel="stylesheet" type="text/javascript"/>
-                </body>
-                </html>
+            </table>
+        </div>
+        <link href="../recources/js/bootstrap.min.js" rel="stylesheet" type="text/javascript"/>
+    </body>
+</html>
