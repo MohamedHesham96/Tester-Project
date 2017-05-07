94e497f6414f411c8dee9943a75c5e2b30d05fa0
MoussaElenany95 <mousa.elenanyfciscu@gmail.com>
2017-05-07 23:27:58 +0200

    Update Query of Followers Operation to get all user data

--- controller/FollowersOperations.php
+++ controller/FollowersOperations.php
@@ -3,9 +3,9 @@ class FollowersOperations {
     public static function getAllFollowers($userid) {
         
         include '../include/vars.php';
-        $conn = new mysqli($host, $username, $password, $dbname);
-        $query = "SELECT student_name FROM following WHERE doctor_id = '$userid'";
-        
+        $conn  = new mysqli($host, $username, $password, $dbname);
+        $query = "SELECT * FROM users WHERE username IN (SELECT student_name FROM following WHERE doctor_id = '$userid')";
+       
         $result = mysqli_query($conn, $query);
         if (mysqli_error($conn)) {
             echo 'Followers Error !!';
