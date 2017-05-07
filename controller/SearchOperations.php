1824dbaa1c67f46cc3d15fb2e2432e3b97153a6d
mohamedhesham96 <H@192.168.1.113>
2017-05-07 03:46:42 +0300

    Correct Search Logic

--- controller/SearchOperations.php
+++ controller/SearchOperations.php
@@ -3,13 +3,25 @@
 class SearchOperations {
 
     public static function getSearchResult($search) {
+
         include '../include/vars.php';
         $conn = new mysqli($host, $username, $password, $dbname);
 
-        //here search by code and name 
-        $query = "SELECT quizzes.quiz_id, quizzes.quiz_name, users.username, quizzes.password from quizzes "
-                . "JOIN users on users.id = quizzes.doctor_id where  (quiz_name LIKE '%" . $search . "%' or quiz_id = '" . $search . "') and quizzes.state = 'Opend'";
+        if ($_SESSION['usertype'] != 'admin') {
+            //here search by code and name 
+            $studentName = $_SESSION['username'];
+            $query = "SELECT quizzes.quiz_id, quizzes.quiz_name, users.username, quizzes.password from quizzes "
+                    . "JOIN users on users.id = quizzes.doctor_id where  quizzes.quiz_id Not IN (SELECT history.quiz_id FROM history WHERE history.student_name = '$studentName') and (quiz_name LIKE '%" . $search . "%' or quiz_id = '" . $search . "') and quizzes.state = 'Opend'";
+        } else {
+
+            $studentName = $_SESSION['username'];
+            $query = "SELECT quizzes.quiz_id, quizzes.quiz_name, users.username, quizzes.password from quizzes "
+                    . "JOIN users on users.id = quizzes.doctor_id where (quiz_name LIKE '%" . $search . "%' or quiz_id = '" . $search . "')";
+        }
+
+
         $result = mysqli_query($conn, $query);
+
         if (mysqli_error($conn)) {
             echo 'error ';
             return NULL;
--- view/SearchPage.php
+++ view/SearchPage.php
@@ -1,19 +1,16 @@
 <?php include './Header.php'; ?>
 <html>
     <head>
-  
+
         <meta charset="utf-8"/>
     </head>
     <body >
         <?php
-            if(isset($_GET['SearchSub']))
-            {    
-                $search = $_GET['Search'];
-            } 
-            else 
-            {
-                die('<h3 style="text-align:center;">No search reuslt found</h2>');
-            }
+        if (isset($_GET['SearchSub'])) {
+            $search = $_GET['Search'];
+        } else {
+            die('<h3 style="text-align:center;">No search reuslt found</h2>');
+        }
         ?>
 
         <div class="container">
@@ -22,56 +19,54 @@
             <h1> Results </h1>
 
             <table class="containerr"> 
-                
-
-
-                <?php
-                include '../controller/SearchOperations.php';
-                $reult = SearchOperations::getSearchResult($search);
 
 
 
-                // check if the statment is true
+<?php
+include '../controller/SearchOperations.php';
+$reult = SearchOperations::getSearchResult($search);
 
-                if (!$reult) {
-                    echo 'error2';
-                } else {
-                        if( $reult->num_rows > 0)
-                        {    
-                        echo  '  <tr>
+
+
+// check if the statment is true
+
+if (!$reult) {
+    echo 'error2';
+} else {
+    if ($reult->num_rows > 0) {
+        echo '  <tr>
                                     <td>Quiz Code</td>
                                     <td>Quiz Name</td>
                                     <td>Doctor Name</td>
                                     <td>Password</td>
 
                               </tr>';
-                            while ($row = mysqli_fetch_array($reult, 1)) {
-                            $quizid = $row['quiz_id'];
-                            $quizName = $row['quiz_name'];
-                            $doctorName = $row['username'];
+        while ($row = mysqli_fetch_array($reult, 1)) {
+            $quizid = $row['quiz_id'];
+            $quizName = $row['quiz_name'];
+            $doctorName = $row['username'];
 
-                            echo '<tr >';
-                            if ($_SESSION['usertype'] != "doctor")
-                                echo "<td><a href='Quiz.php?id=$quizid&name=$quizName&maker=$doctorName'>" . $quizid . "</a></td>";
-                            else
-                                echo "<td>" . $quizid . "</td>";
+            echo '<tr >';
+            if ($_SESSION['usertype'] != "doctor")
+                echo "<td><a href='Quiz.php?id=$quizid&name=$quizName&maker=$doctorName'>" . $quizid . "</a></td>";
+            else
+                echo "<td>" . $quizid . "</td>";
 
-                            echo "<td>" . $quizName . "</td>";
-                            echo "<td>" . $doctorName . "</td>";
+            echo "<td>" . $quizName . "</td>";
+            echo "<td>" . $doctorName . "</td>";
 
-                            if ($row['password']) {
-                                echo "<td>" . '<img src="../recources/images/1.png"  height="20" width="20">' . "</td>";
-                            } else {
-                                echo "<td>" . '<img src="../recources/images/0.png"  height="22" width="22">' . "</td>";
-                            }
-                            echo "</tr>";
-                            }
-                        }
-                        else {
-                            die('<h3 style="text-align:center;">No search reuslt found</h2>');
-                        }
-                }
-                ?>
+            if ($row['password']) {
+                echo "<td>" . '<img src="../recources/images/1.png"  height="27">' . "</td>";
+            } else {
+                echo "<td>" . '<img src="../recources/images/0.png"  height="27">' . "</td>";
+            }
+            echo "</tr>";
+        }
+    } else {
+        die('<h3 style="text-align:center;">No search reuslt found</h2>');
+    }
+}
+?>
 
             </table>
         </div>
