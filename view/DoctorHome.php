96b8e8ff86a22d7a5da2568b9a505418a71fc291
Mahmoud samir <Mahmoud samir@MAHMOUD>
2017-05-08 18:35:07 +0200

    Update

--- view/DoctorHome.php
+++ view/DoctorHome.php
@@ -24,7 +24,7 @@
             $conn = mysqli_connect($host, $username, $password, $dbname);
             if ($conn->error)
                 die("connection lost");
-            $sql = "SELECT quizzes.quiz_id, quizzes.quiz_name, users.username, quizzes.password from quizzes JOIN users on users.id = quizzes.doctor_id where quizzes.state = 'opend'";
+            $sql = "SELECT quizzes.quiz_id, quizzes.quiz_name, users.username, quizzes.password from quizzes JOIN users on users.id = quizzes.doctor_id where quizzes.state = 'opened'";
             $result = $conn->query($sql);
             if (!$result)
                 die($conn->error);
