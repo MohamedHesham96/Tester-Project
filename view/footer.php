18f8c847c545b54cdac2f4988e8d06c8120fad18
mohamed abouzaid <mohamed.abozaid39@yahoo.com>
2017-05-10 06:54:19 +0200

    add footer to doctor page

---
+++ view/footer.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    </style>
+    <title></title>
+  </head>
+  <body>
+    <footer >
+                           <div class="container">
+                               <div class="row">
+                                   <div class="col-md-10">
+                                       <ul class="footer-menu">
+                                           <li><a href="#">Home</a></li>
+                                           <li><a href="#">About Us</a></li>
+                                           <li><a href="#">Contact Us</a></li>
+                                         <li><a href="#">Privacy & Terms</a></li>
+                                       </ul>
+                                   </div>
+                                   <br>
+                                   <div>
+                                       &copy;Copyright - Online Quizer. All Rights Reserved
+
+                                   </div>
+                               </div>
+                           </div>
+
+       </footer>
+
+  </body>
+</html>
