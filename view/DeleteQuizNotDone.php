36bac81d9a448f00726c6b4366a44c27a32b6de9
mohamed abouzaid <mohamed.abozaid39@yahoo.com>
2017-05-11 07:20:30 +0200

    add design to add new question and delete question and finish quiz

--- view/DeleteQuizNotDone.php
+++ view/DeleteQuizNotDone.php
@@ -1,9 +1,22 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>delete quiz</title>
+  </head>
+  <body>
+    <?php
+
+    include '../controller/CreateQuizOperations.php';
+
+    session_start();
+    $user = $_SESSION['username'];
+    $quizID = CreateQuizOperations::getQuizID($user);
+    CreateQuizOperations::deleteQuiz($quizID);
+
+    echo '<script>document.location.href="DoctorHome.php?"</script>';?>
+    <?php include './footer.php';
+    ?>
+  </body>
+</html>
 <?php
-
-include '../controller/CreateQuizOperations.php';
-session_start();
-$user = $_SESSION['username'];
-$quizID = CreateQuizOperations::getQuizID($user);
-CreateQuizOperations::deleteQuiz($quizID);
-
-echo '<script>document.location.href="DoctorHome.php?"</script>';
--- view/FinishQuiz.php
+++ view/FinishQuiz.php
@@ -1,32 +1,44 @@
-<?php
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>finish quiz</title>
+  </head>
+  <body>
+    <?php
 
-include '../controller/CreateQuizOperations.php';
+    include '../controller/CreateQuizOperations.php';
 
-session_start();
+    session_start();
 
-$quizID = CreateQuizOperations::getQuizID($_SESSION['username']);
-
-if (!$quizID) {
-    CreateQuizOperations::addQuiz();
     $quizID = CreateQuizOperations::getQuizID($_SESSION['username']);
-}
 
-$header = $_GET['question'];
-$ans1 = $_GET['ans1'];
-$ans2 = $_GET['ans2'];
-$ans3 = '';
-$ans4 = '';
+    if (!$quizID) {
+        CreateQuizOperations::addQuiz();
+        $quizID = CreateQuizOperations::getQuizID($_SESSION['username']);
+    }
 
-if (isset($_GET['ans3'])) {
-    $ans3 = $_GET['ans3'];
-}
+    $header = $_GET['question'];
+    $ans1 = $_GET['ans1'];
+    $ans2 = $_GET['ans2'];
+    $ans3 = '';
+    $ans4 = '';
 
-if (isset($_GET['ans4'])) {
-    $ans4 = $_GET['ans4'];
-}
+    if (isset($_GET['ans3'])) {
+        $ans3 = $_GET['ans3'];
+    }
 
-$corectAns = $_GET['correctans'];
+    if (isset($_GET['ans4'])) {
+        $ans4 = $_GET['ans4'];
+    }
 
-$quizID = CreateQuizOperations::addQuestion($quizID, $header, $ans1, $ans2, $ans3, $ans4, $corectAns);
+    $corectAns = $_GET['correctans'];
 
-echo '<script>document.location.href="CreateQuiz.php?submitstate=true"</script>';
+    $quizID = CreateQuizOperations::addQuestion($quizID, $header, $ans1, $ans2, $ans3, $ans4, $corectAns);
+
+    echo '<script>document.location.href="CreateQuiz.php?submitstate=true"</script>';?>
+    <?php include './footer.php';
+    ?>
+
+  </body>
+</html>
